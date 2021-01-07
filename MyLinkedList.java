c7d10569eb2e155618cbeff3a9eca96a73987a96
adnanq2004 <aquayyum20@stuy.edu>
2021-01-06 19:00:36 -0500

    possibly finished making all of hte methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,38 +1,96 @@
 public class MyLinkedList{
+
  private int size;
+
  private Node start,end;
+
  public MyLinkedList(int s, Node st, Node en){/*create a constructor*/
+ 
    size = s;
    start = st;
    end = en;
    start.setNext(end);
    end.setPrev(start);
+
  }
+
  public int size() {
+
    return size;
+
  }
+
  public boolean add(String value) {
+
    Node n = new Node(value);
    end.setNext(n);
    n.setPrev(end);
    size++;
    return true;
+
  }
- public boolean add(int index, String value) {
+
+ public void add(int index, String value) {
+
    Node current = start;
    for (int i = 0; i < size; i++) {
      if (i == index) {
        Node n = new Node(value);
-       current.getPrev().setNext(n);
-       n.setPrev(current.getPrev());
+       current.prev().setNext(n);
+       n.setPrev(current.prev());
        n.setNext(current);
        current.setPrev(n);
      }
-     current = current.getNext();
+     current = current.next();
    }
+
  }
- public String get(int index);
- public String set(int index, String value);
- public String toString();
+
+ public String get(int index) {
+
+	Node current = start;
+	for (int count = 0; count < index;count++) {
+		current = current.next();
+	}
+	return current.value();
+
+ }
+
+ public String set(int index, String value) {
+
+	 Node current = start;
+	 for (int count = 0; count < index; count++) {
+		current = current.next();
+	 }
+	 String val = current.value();
+	 current.setValue(value);
+	 return val;
+	 
+
+ }
+
+ public String toString() {
+	
+	Node current = start;
+	String val = "";
+	while (current.next().value() != null) {
+		val += current.value();
+		val += ", ";
+	}
+	val += end.value();
+	return val;
+ }
+ 
+
+	private Node getN(int n) {
+		
+		Node current = start;
+		for (int i = 0; i < n; i++) {
+			current = current.next();
+		}
+		return current;
+
+	}
+
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
